b79df259d01fd75bc529e3130fad685e7cb8c5c2
Sharon <naisiayee@gmail.com>
2026-02-23 23:50:40 -0500

    implemented from/to inputs and added validation,payload handling and callback support

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,14 @@
-import NavBar from '../components/NavBar'
-import FromToCard from '../components/FromToCard'
-import MapCard from '../components/MapCard'
-import WeatherCard from '../components/WeatherCard'
+import NavBar from "../components/NavBar";
+import FromToCard from "../components/FromToCard";
+import MapCard from "../components/MapCard";
+import WeatherCard from "../components/WeatherCard";
 
 const Home = () => {
   return (
     <div className="home-layout">
-      <NavBar />
       <FromToCard />
-      <MapCard />
-      <WeatherCard />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
