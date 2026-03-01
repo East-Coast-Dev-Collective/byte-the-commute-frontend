e13ec7c2cd06d13ad10dc19ddda67e2d4c9c0106
Sharon <naisiayee@gmail.com>
2026-02-28 23:45:18 -0500

    rendered navbar in home,updated styling and added profile placeholder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,28 @@
-const NavBar = () => {
-  return (
-    <nav>
-      <h2>NavBar Placeholder</h2>
-    </nav>
-  )
-}
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
-export default NavBar
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__left">
+        <div className="navbar__brand">
+          <img
+            src={logo}
+            alt="Byte The Commute Logo"
+            className="navbar__logo"
+          />
+
+          <NavLink to="/" className="navbar__title">
+            Byte The Commute
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="navbar__right">
+        <div className="navbar__profile" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
